ea1cc83c4956cba48d2eb1262f10e5c0c696ba52
Jordy Figueroa <jordyfigueroa93@icloud.com>
2023-05-30 18:40:14 -0700

    1st commit starting with mobile design first

---
+++ src/layout/navbar.js
@@ -0,0 +1,15 @@
+import closedIcon from '../images/Property 1=Closed.png'
+import openIcon from '../images/Property 1=Open.png'
+import logo from '../images/logos/pickEmLogoR.png'
+import classes from './Navbar.css'
+
+const navbar = () => {
+    return (
+        <navbar>
+            <img className='navLogo' src={logo}/>
+            <img/>
+        </navbar>
+    )
+}
+
+export default navbar
