ebaeb8ef1b20267833abe704901d0b6917ac6dad
aydcereal <jordyfigueroa93@icloud.com>
2023-08-01 21:31:40 -0700

    display name on navbar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,9 +1,37 @@
 import Navlinks from "./navLinks";
 import { NavLink, useNavigate } from "react-router-dom";
+import AuthContext from "../context/auth-context";
+import { useState } from "react";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect } from "react";
+import { useContext } from "react";
+import arrow from '../images/down_arrow.png'
 
+
+
+const db = getDatabase();
+
+const getUserData = (userId, callback) => {
+    const userRef = ref(db, `users/${userId}`);
+    onValue(userRef, (snapshot) => {
+      const userData = snapshot.val();
+      callback(userData);
+    });
+  };
 
 
 const Navigation = ({isAuthenticated, onLogout, logoutHandler }) => {
+
+    const { currentUser } = useContext(AuthContext);
+    const [userData, setUserData] = useState(null);
+    
+
+    useEffect(() => {
+        if (currentUser) {
+            console.log(currentUser);
+          getUserData(currentUser.uid, setUserData);
+        }
+      }, [currentUser]);
 
     
     return(
@@ -13,12 +41,16 @@ const Navigation = ({isAuthenticated, onLogout, logoutHandler }) => {
             
         <Navlinks/>
         {isAuthenticated ? (
-            <button onClick={event => {
+            <div onClick={event => {
                 onLogout(); 
                 logoutHandler();
-            }} className="btn btn-lg btn-danger nav-btn">
-                Logout
-            </button>
+            }} >
+                <a className="userMenu">
+                {userData && userData.displayName}
+                <img src={arrow}/>
+                </a>
+               
+            </div>
         ) :<NavLink  className='btn btn-lg btn-danger nav-btn' to='/login' >
             Login
             </NavLink> }
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,38 +3,20 @@ import AuthContext from "../context/auth-context"
 import { useContext, useState, useEffect } from "react"
 import { getDatabase, ref, onValue } from "firebase/database";
 
-const db = getDatabase();
 
-const getUserData = (userId, callback) => {
-    const userRef = ref(db, `users/${userId}`);
-    onValue(userRef, (snapshot) => {
-      const userData = snapshot.val();
-      callback(userData);
-    });
-  };
 
 
 export default function  Dashboard () {
-    const { currentUser } = useContext(AuthContext);
-    const [userData, setUserData] = useState(null);
+    
 
 
-    useEffect(() => {
-        if (currentUser) {
-          getUserData(currentUser.uid, setUserData);
-        }
-      }, [currentUser]);
+   
 
 
     return (
     <div>
       <h1 className="pageTitle">Dashboard</h1>
-      {userData && (
-        <div>
-          <p>Display Name: {userData.displayName}</p>
-          {/* other user data */}
-        </div>
-      )}
+     
     </div>
   );
 }
