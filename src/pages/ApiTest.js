0f32840271dc114f5cfe29143599ffa32d0e92e8
aydcereal <jordyfigueroa93@icloud.com>
2023-08-29 22:22:40 -0700

    mapped every match

--- src/pages/ApiTest.js
+++ src/pages/ApiTest.js
@@ -1,29 +1,50 @@
 import React, { useState, useEffect } from 'react';
 
-function ApiTest() {
-  const [teamNicknames, setTeamNicknames] = useState([]);
+const ApiTest = () => {
+  const [matches, setMatches] = useState([]);
 
   useEffect(() => {
-    fetch('https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?seasontype=2&week=1&dates=2023')
+    const API_ENDPOINT_URL = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?seasontype=2&week=1&dates=2023";
+
+    fetch(API_ENDPOINT_URL)
       .then(response => response.json())
       .then(data => {
-        console.log(data.events[0].competitions[0].competitors[0].team.location);
-        const competition = data.events[0].competitions[0];
-        const teamNicknames = competition.competitors.map(competitor => competitor.team.location);
-        setTeamNicknames(teamNicknames);
+        const events = data.events || [];
+
+        const matchData = [];
+
+        events.forEach(event => {
+          const competitions = event.competitions || [];
+
+          competitions.forEach(competition => {
+            const competitors = competition.competitors;
+            if (competitors.length === 2) {
+              const team1 = competitors[0].team.location;
+              const team2 = competitors[1].team.location;
+              matchData.push({ team1, team2 });
+            }
+          });
+        });
+
+        setMatches(matchData);
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
       });
   }, []);
 
   return (
     <div>
-      <h1>Team Nicknames</h1>
+      <h1>NFL Match Schedule</h1>
       <ul>
-        {teamNicknames.map(nickname => (
-          <li key={nickname}>{nickname}</li>
+        {matches.map((match, index) => (
+          <li key={index}>
+            {match.team1} vs {match.team2}
+          </li>
         ))}
       </ul>
     </div>
   );
-}
+};
 
 export default ApiTest;
