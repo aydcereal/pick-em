5892c1125426c09a7ec50823c866800dcd5a9fb4
aydcereal <jordyfigueroa93@icloud.com>
2023-11-14 23:13:56 -0800

    svg

--- src/pages/Picks.js
+++ src/pages/Picks.js
@@ -16,7 +16,11 @@ const Picks = () => {
                         <div className="header row">
                             <div className="pie-progress-charts col-md-2 col-4">
                                 <div className="inner-pchart">
-                                    <div className='circle'></div>
+                                    <div className='circle'>
+                                        <div className="circles-wrp">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70"><path fill="transparent" stroke="#ddd" stroke-width="4" d="M 34.99327878427806 2.0000006844657747 A 33 33 0 1 1 34.95416366204364 2.000031832892283 Z"></path><path fill="transparent" stroke="#999" stroke-width="4" d="M 34.99327878427806 2.0000006844657747 A 33 33 0 1 1 34.95416366204364 2.000031832892283 "></path></svg>
+                                        </div>
+                                    </div>
                                     <h3 className="circle-title">Active Entries</h3>
                                     <p className="noprint"><a >View all members</a></p>
                                 </div>
