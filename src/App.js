997e35bc2903b21b89d1370275c5dd7b240269cf
aydcereal <jordyfigueroa93@icloud.com>
2023-11-27 18:47:34 -0800

    basename configuration

--- src/App.js
+++ src/App.js
@@ -1,92 +1,108 @@
+import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import { AuthProvider } from "./context/auth-context";
+import AuthContext from "./context/auth-context";
+import { useContext } from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-import './App.css';
-import { createBrowserRouter, RouterProvider,Outlet, Navigate } from 'react-router-dom';
-import { AuthProvider } from './context/auth-context';
-import AuthContext from './context/auth-context';
-import { useContext } from 'react';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-
-import Homepage from './pages/homepage';
-import Login from './pages/login';
-import Signup from './pages/signUp';
-import Dashboard from './pages/dashboard';
-import ChoosePassword from './pages/choosePassword';
-import RootLayout from './pages/Root';
-import SignupRoot from './pages/SignupRoot';
-import ErrorPage from './pages/Error';
-import UserDetailsPage from './pages/UserDetailsPage';
-import NewPool from './pages/NewPool';
-import Connections from './pages/connections';
-import ApiTest from './pages/ApiTest';
-import PoolDetail from './pages/PoolDetail';
-import ManageEntries from './components/ManageEntries';
-import Invite from './pages/Invite';
-import InviteLandingPage from './pages/InviteLandingPage';
-import Picks from './pages/Picks';
-
-
-
-
+import Homepage from "./pages/homepage";
+import Login from "./pages/login";
+import Signup from "./pages/signUp";
+import Dashboard from "./pages/dashboard";
+import ChoosePassword from "./pages/choosePassword";
+import RootLayout from "./pages/Root";
+import SignupRoot from "./pages/SignupRoot";
+import ErrorPage from "./pages/Error";
+import UserDetailsPage from "./pages/UserDetailsPage";
+import NewPool from "./pages/NewPool";
+import Connections from "./pages/connections";
+import ApiTest from "./pages/ApiTest";
+import PoolDetail from "./pages/PoolDetail";
+import ManageEntries from "./components/ManageEntries";
+import Invite from "./pages/Invite";
+import InviteLandingPage from "./pages/InviteLandingPage";
+import Picks from "./pages/Picks";
 
 function ProtectedRoute() {
-  const {isAuthenticated} = useContext(AuthContext);
+  const { isAuthenticated } = useContext(AuthContext);
   if (!isAuthenticated) {
-    return <Navigate to='/login'/>
+    return <Navigate to="/login" />;
   }
-  return <Outlet />
+  return <Outlet />;
 }
 
-const router = createBrowserRouter([
-  {path: '/', 
-   element:<RootLayout/>,
-   errorElement: <ErrorPage />,
-   children: [
-      {index: true, element: <Homepage/>},
-      {path: '/login', element: <Login/>},
-      {path: '/picks', element: <Picks/>},
-      {path: '/dashboard', element: <ProtectedRoute />, children: [{index:
-        true, element:<Dashboard />}]},
-      {path: '/selections/:poolId', element: <ManageEntries/>},
-      
-      {path: '/dashboard/my-pools', element: <ProtectedRoute />, children: [{index:
-        true, element:<Dashboard />}]},
-      
-      { path: '/pools/:poolKey/:week',children: [
-          { index: true, element: <PoolDetail /> }
-        ]},
-      
-      {path: '/pools/:poolId/invite', element:<Invite/> },
+const router = createBrowserRouter({
+  basename: process.env.PUBLIC_URL,
+  routes: [
+    {
+      path: "/",
+      element: <RootLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        { index: true, element: <Homepage /> },
+        { path: "/login", element: <Login /> },
+        { path: "/picks", element: <Picks /> },
+        {
+          path: "/dashboard",
+          element: <ProtectedRoute />,
+          children: [{ index: true, element: <Dashboard /> }],
+        },
+        { path: "/selections/:poolId", element: <ManageEntries /> },
 
-      {path: '/join', element: <InviteLandingPage />},
-      {path: '/Picks', element: <Picks />},
+        {
+          path: "/dashboard/my-pools",
+          element: <ProtectedRoute />,
+          children: [{ index: true, element: <Dashboard /> }],
+        },
 
+        {
+          path: "/pools/:poolKey/:week",
+          children: [{ index: true, element: <PoolDetail /> }],
+        },
 
-      {path: '/pools/start', element: <ProtectedRoute />, children: [{index:
-          true, element:<NewPool />}]},
-      {path: '/connections', element: <ProtectedRoute />, children: [{index:
-          true, element:<Connections />}]},
-      {path: '/signup', element: <SignupRoot/>,
-        children: [
-        {path: '', element:<Signup/>},
-        {path:'password', element:<ChoosePassword/>},
-        {path:'user-details', element: <UserDetailsPage />}
-      ]},
-      
-    ]}
-  
-])
+        { path: "/pools/:poolId/invite", element: <Invite /> },
 
+        { path: "/join", element: <InviteLandingPage /> },
+        { path: "/Picks", element: <Picks /> },
+
+        {
+          path: "/pools/start",
+          element: <ProtectedRoute />,
+          children: [{ index: true, element: <NewPool /> }],
+        },
+        {
+          path: "/connections",
+          element: <ProtectedRoute />,
+          children: [{ index: true, element: <Connections /> }],
+        },
+        {
+          path: "/signup",
+          element: <SignupRoot />,
+          children: [
+            { path: "", element: <Signup /> },
+            { path: "password", element: <ChoosePassword /> },
+            { path: "user-details", element: <UserDetailsPage /> },
+          ],
+        },
+      ],
+    },
+  ],
+});
 
 function App() {
-  return <AuthProvider>
-    <Provider store={store}>
-      <RouterProvider router={router}/>
-    </Provider>
-  </AuthProvider>
-  
-  
-  
+  return (
+    <AuthProvider>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </AuthProvider>
+  );
 }
 
 export default App;
